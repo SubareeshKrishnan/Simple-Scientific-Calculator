9a1a7a2b3d6c04f5296abdf8ee11b291f051f326
Subareesh <subareeshkrishnan@gmail.com>
2020-07-05 22:17:59 +0530

    Simple Scientific calculator.

---
+++ app/src/main/java/com/orion/subareesh/Feedback_activity.kt
@@ -0,0 +1,17 @@
+package com.orion.subareesh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Feedback_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feedback_activity)
+
+
+    }
+}
+
+
+
